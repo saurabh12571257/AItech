c97a19ef6d4f6c56bda51bddddd90fa68fc06ba2
saurabh12571257 <davesaurabh59@gmail.com>
2025-04-25 19:40:34 +0530

    Interview Page insights and recent quizzes card archi

--- app/(main)/interview/_components/quiz-list.jsx
+++ app/(main)/interview/_components/quiz-list.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+"use client";
 
-const QuizList = () => {
-  return (
-    <div>
-      QuizList
-    </div>
-  )
-}
+import { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { Button } from '@/components/ui/button';
+
+
+const QuizList = ({assessments}) => {
+
+    const router = useRouter();
+    const [selectQuiz, setSelectedQuiz] = useState(null);
+
+  return(
+  <>
+    <Card className="mt-4">
+      <CardHeader>
+        <div>
+          <CardTitle className="gradient-title text-3xl md:text-4xl">
+            Recent Quizzes
+          </CardTitle>
+          <CardDescription className="mb-2">
+            Review your past quiz performance.
+          </CardDescription>
+        </div>
+        <Button onClick={() => router.push("/interview/mock")}> 
+            Start new quiz
+        </Button>
+      </CardHeader>
+      <CardContent>
+        <p> 
+            Card Content 
+        </p>
+      </CardContent>
+    </Card>
+  </>
+
+)};
 
 export default QuizList;
